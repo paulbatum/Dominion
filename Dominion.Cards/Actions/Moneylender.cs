24f2c181d5c5bb3c186c3d1101bd442320a6e6cf
unknown <Anatoli@.(none)>
2010-11-13 01:48:06 +1100

    Added moneylender

---
+++ Dominion.Cards/Actions/Moneylender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dominion.Rules;
+using Dominion.Rules.Activities;
+using Dominion.Rules.CardTypes;
+using Dominion.Cards.Treasure;
+
+namespace Dominion.Cards.Actions
+{
+    class Moneylender : Card, IActionCard
+    {
+        public Moneylender()
+            : base(4)
+        { }
+
+        public void Play(TurnContext context)
+        {
+            context.AddEffect(new MoneylenderEffect());
+        }
+
+        private class MoneylenderEffect : CardEffectBase
+        {
+            public override void Resolve(TurnContext context)
+            {
+                if (context.ActivePlayer.Hand.OfType<Copper>().Any())
+                {
+                    var copperCard = context.ActivePlayer.Hand.OfType<Copper>().FirstOrDefault();
+                    context.Trash(context.ActivePlayer, copperCard);
+                    context.AvailableSpend += 3;
+                };
+            }
+        }
+    }
+}
