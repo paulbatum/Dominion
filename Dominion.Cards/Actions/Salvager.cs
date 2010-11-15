f45e50fcfed8da70584ef442f69c0dba46abbc11
anatolilightfoot <anatoli.lightfoot@gmail.com>
2010-11-15 22:29:08 +1100

    Implemented Salvager.

---
+++ Dominion.Cards/Actions/Salvager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dominion.Rules;
+using Dominion.Rules.Activities;
+using Dominion.Rules.CardTypes;
+
+namespace Dominion.Cards.Actions
+{
+    class Salvager : Card, IActionCard
+    {
+        public Salvager()
+            : base(4)
+        {
+        }
+
+        public void Play(TurnContext context)
+        {
+            context.Buys += 1;
+            context.AddEffect(new SalvagerEffect());
+        }
+
+        private class SalvagerEffect : CardEffectBase
+        {
+            public override void Resolve(TurnContext context)
+            {
+                var activity = new SelectCardsActivity(context, "Select a card to salvage",
+                    SelectionSpecifications.SelectExactlyXCards(1));
+
+                activity.AfterCardsSelected = cardList =>
+                {
+                    var cardToSalvage = cardList.Single();
+                    context.AvailableSpend += cardToSalvage.Cost;
+                    context.Trash(context.ActivePlayer, cardToSalvage);
+                };
+
+                _activities.Add(activity);
+            }
+        }
+    }
+}
---
+++ Dominion.Specs/Cards/Salvager.feature
@@ -0,0 +1,16 @@
+﻿Feature: Salvager
+
+Background:
+	Given A new game with 3 players	
+	And Player1 has a Warehouse in hand instead of a Copper
+	When Player1 plays a Salvager
+
+Scenario: Salvager trashes a card
+	Then Player1 must select 1 card to trash
+
+Scenario: Salvager trashing estate gives plus 2 spend
+	And Player1 selects a Estate to trash
+	Then Player1 should have 2 to spend
+
+Scenario: Salvager gives plus one buy
+	Then Player1 should have 2 buys
