e95a151e37bd080a943eec9accd8db603d32efa2
anatolilightfoot <anatoli.lightfoot@gmail.com>
2010-11-20 18:37:30 +1100

    Implemented Adventurer.

---
+++ Dominion.Cards/Actions/Adventurer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dominion.Rules;
+using Dominion.Rules.Activities;
+using Dominion.Rules.CardTypes;
+
+namespace Dominion.Cards.Actions
+{
+    public class Adventurer : Card, IActionCard
+    {
+        public Adventurer() : base(6)
+        {
+            
+        }
+
+        public void Play(TurnContext context)
+        {
+            context.AddEffect(new AdventurerEffect());                                
+        }
+
+        private class AdventurerEffect : CardEffectBase
+        {
+            private IEnumerable<ITreasureCard> MatchingCards(RevealZone zone)
+            {
+                return zone.OfType<ITreasureCard>();
+            }
+
+            public override void Resolve(TurnContext context)
+            {
+                var deck = context.ActivePlayer.Deck;
+                var revealZone = new RevealZone(context.ActivePlayer);
+
+                while (deck.TopCard != null && MatchingCards(revealZone).Count() < 2)
+                    deck.TopCard.MoveTo(revealZone);
+
+                revealZone.LogReveal(context.Game.Log);
+                var revealedTreasure = MatchingCards(revealZone).ToList();
+
+                var discards = revealZone.Where(c => !revealedTreasure.Cast<ICard>().Contains(c)).ToList();
+
+                foreach(var card in discards)
+                    card.MoveTo(context.ActivePlayer.Discards);
+
+                foreach (var card in revealedTreasure)
+                    card.MoveTo(context.ActivePlayer.Hand);
+
+            }
+        }
+    }
+}
---
+++ Dominion.Specs/Cards/Adventurer.feature
@@ -0,0 +1,28 @@
+﻿Feature: Adventurer
+
+Scenario: Play Adventurer but reveal no treasure
+	Given A new game with 3 players
+	And Player1 has a hand of Adventurer, Copper, Copper, Copper, Copper
+	And Player1 has a deck of Estate, Estate, Estate, Estate, Estate
+	When Player1 plays a Adventurer
+	Then Player1 should have a hand of Copper, Copper, Copper, Copper
+	And Player1 should have 0 cards in deck
+	And Player1 should have a discard pile of Estate, Estate, Estate, Estate, Estate
+
+Scenario: Play Adventurer but reveal only one treasure
+	Given A new game with 3 players
+	And Player1 has a hand of Adventurer, Copper, Copper, Copper, Copper
+	And Player1 has a deck of Estate, Estate, Estate, Copper, Estate
+	When Player1 plays a Adventurer
+	Then Player1 should have a hand of Copper, Copper, Copper, Copper, Copper
+	And Player1 should have 0 cards in deck
+	And Player1 should have a discard pile of Estate, Estate, Estate, Estate
+
+Scenario: Play Adventurer and reveal two treasure
+	Given A new game with 3 players
+	And Player1 has a hand of Adventurer, Copper, Copper, Copper, Copper
+	And Player1 has a deck of Estate, Copper, Estate, Copper, Silver
+	When Player1 plays a Adventurer
+	Then Player1 should have a hand of Copper, Copper, Copper, Copper, Copper, Copper
+	And Player1 should have a deck of Silver
+	And Player1 should have a discard pile of Estate, Estate
