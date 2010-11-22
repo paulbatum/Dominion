e67a31f1262c0b819449a0713fc623adb279cb68
anatolilightfoot <anatoli.lightfoot@gmail.com>
2010-11-22 18:45:27 +1100

    Implemented Sea Hag.

---
+++ Dominion.Cards/Actions/SeaHag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dominion.Rules;
+using Dominion.Rules.Activities;
+using Dominion.Rules.CardTypes;
+using Dominion.Cards.Curses;
+
+namespace Dominion.Cards.Actions
+{
+    class SeaHag : Card, IActionCard, IAttackCard
+    {
+        public SeaHag() : base(4) { }
+
+        public void Play(TurnContext context)
+        {
+            context.AddEffect(new SeaHagAttack());
+        }
+
+        private class SeaHagAttack : AttackEffect
+        {
+            public override void Attack(Player player, TurnContext context)
+            {
+                player.Deck.MoveTop(1, player.Discards);
+                var gainUtil = new GainUtility(context, player);
+                gainUtil.Gain<Curse>(c => c.MoveTo(player.Deck));
+            }
+        }
+    }
+}
---
+++ Dominion.Specs/Cards/SeaHag.feature
@@ -0,0 +1,11 @@
+﻿Feature: SeaHag
+
+Scenario: Play sea hag
+	Given A new game with 3 players	
+	And Player1 has a SeaHag in hand instead of a Copper
+	When Player1 plays a SeaHag
+	Then Player1 should have 4 cards in hand
+	Then Player2 should have a Curse on top of the deck
+	Then Player2 should have a card on top of the discard pile
+	Then Player3 should have a Curse on top of the deck
+	Then Player3 should have a card on top of the discard pile
