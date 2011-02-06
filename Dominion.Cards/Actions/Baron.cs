a2c3cd6ac08101119c0d34ff7fbd8c25a2e12944
Paul Batum <paul.batum@gmail.com>
2011-02-05 17:01:58 -0800

    Implemented Baron

---
+++ Dominion.Cards/Actions/Baron.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Dominion.Cards.Victory;
+using Dominion.Rules;
+using Dominion.Rules.Activities;
+using Dominion.Rules.CardTypes;
+
+namespace Dominion.Cards.Actions
+{
+    public class Baron : Card, IActionCard
+    {
+        public Baron()
+            : base(4)
+        { }
+
+        public void Play(TurnContext context)
+        {
+            context.AddEffect(this, new BaronEffect());
+            context.Buys += 1;
+        }
+
+        public class BaronEffect : CardEffectBase
+        {
+            public override void Resolve(TurnContext context, ICard source)
+            {
+                var estate = context.ActivePlayer.Hand.OfType<Estate>().FirstOrDefault();
+
+                Action discardAction = () =>
+                {
+                    context.AvailableSpend += 4;
+                    context.DiscardCard(context.ActivePlayer, estate);
+                };
+
+                Action gainAction = () => new GainUtility(context, context.ActivePlayer).Gain<Estate>();
+
+                if (estate != null)
+                {
+                    var activity = Activities.ChooseYesOrNo
+                        (context.Game.Log, context.ActivePlayer, "Discard an Estate?",
+                         source,
+                         discardAction, gainAction);
+
+                    _activities.Add(activity);
+                }
+                else
+                {
+                    gainAction();
+                }
+            }
+        }
+    }
+}
---
+++ Dominion.Specs/Cards/Baron.feature
@@ -0,0 +1,31 @@
+﻿Feature: Baron	
+
+Scenario: Player plays baron with no estate in hand
+	Given A new game with 3 players	
+	And Player1 has a hand of Baron, Copper, Copper, Copper, Copper
+	When Player1 plays a Baron
+	Then Player1 should have an Estate on top of the discard pile
+	And Player1 should have 2 buys
+
+Scenario: Player plays baron with an estate in hand and must choose
+	Given A new game with 3 players	
+	And Player1 has a hand of Baron, Estate, Copper, Copper, Copper
+	When Player1 plays a Baron
+	Then Player1 must choose whether to use Chancellor's effect (Yes or No)	
+
+Scenario: Player plays baron with an estate in hand and chooses to discard it
+	Given A new game with 3 players	
+	And Player1 has a hand of Baron, Estate, Copper, Copper, Copper
+	When Player1 plays a Baron
+	When Player1 chooses to discard the estate (Yes)
+	Then Player1 should have 4 to spend
+	Then Player1 should have an Estate on top of the discard pile
+	And Player1 should have a hand of Copper, Copper, Copper
+
+Scenario: Player plays baron with an estate in hand and chooses not to discard
+	Given A new game with 3 players	
+	And Player1 has a hand of Baron, Estate, Copper, Copper, Copper
+	When Player1 plays a Baron
+	When Player1 chooses to keep the estate (No)
+	Then Player1 should have 0 to spend
+	And Player1 should have an Estate on top of the discard pile
